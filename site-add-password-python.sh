aefc30bdd7efdc0fb3f3e0cf05ee3a99208722cd
Christine <cj3wilso@gmail.com>
2019-12-24 22:38:45 -0500

    adding the correct virtual server for python

---
+++ site-add-password-python.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo 'No project name provided (mandatory)'
+	exit 1
+else
+	echo "- Project name:" "$1"
+fi
+
+#set the subdomain
+site_url=${2}".christinewilson.ca"
+
+# create conf file
+sudo sh -c 'echo "<VirtualHost *:80>
+	<Directory /var/www/'$1'/public_html>
+		Options +ExecCGI
+		DirectoryIndex index.py
+		AuthType basic
+		AuthName "private"
+		AuthUserFile /etc/apache2/.htpasswd
+		Require valid-user
+	</Directory>
+	AddHandler cgi-script .py
+
+	ServerAdmin info@christinewilson.ca
+	ServerName '$site_url'
+	DocumentRoot /var/www/'$1'/public_html
+
+	ErrorLog \${APACHE_LOG_DIR}/error.log
+	CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>" >> /etc/apache2/sites-available/'$2'.christinewilson.ca.conf'
+
+# restart apache
+echo "Enabling site in Apache... a2ensite $site_url"
+sudo sh -c 'a2ensite '$site_url
+
+echo "Restarting Apache..."
+sudo sh -c 'service apache2 reload'
